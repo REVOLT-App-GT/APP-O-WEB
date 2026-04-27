b931e70dbc64c50b4d2c72ae12e1666447cfc054
REVOLT-App-GT <m1543ljuan2005@gmail.com>
2026-04-27 14:58:15 -0600

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -8,14 +8,13 @@ import { getAnalytics } from "firebase/analytics";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBuJw5oy_MziKk7FiJQP7RNx4wkBbj0d28",
-  authDomain:
-   "revol-t-55396.firebaseapp.com",
+  authDomain: "revol-t-51396.firebaseapp.com",
+  projectId: "revol-t-51396",
   storageBucket: "revol-t-51396.firebasestorage.app",
   messagingSenderId: "920044467992",
   appId: "1:920044467992:web:5c97a4269674ee8964906d",
   measurementId: "G-QJ0VHBGQ94"
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
