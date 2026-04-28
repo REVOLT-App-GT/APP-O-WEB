384156b7047eed1022217cf28e0c4883bd36ef3b
REVOLT-App-GT <m1543ljuan2005@gmail.com>
2026-04-28 14:37:58 -0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,14 +1,13 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
-// Datos extraídos de tu consola de Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyD-PonAquiTuLlaveReal", // BUSCA ESTO EN TU CONSOLA (Imagen abajo)
-  authDomain: "revolt-t-51396.firebaseapp.com",
-  projectId: "revolt-t-51396",
-  storageBucket: "revolt-t-51396.appspot.com",
-  messagingSenderId: "36551823610",
-  appId: "1:36551823610:web:8665c71a36768371306354"
+  apiKey: "AIzaSyADgdGdN4iPwmf8vL1EdYHZfLS9Xp", // Esta es la llave de tu imagen
+  authDomain: "revol-t-51396.firebaseapp.com",
+  projectId: "revol-t-51396",
+  storageBucket: "revol-t-51396.firebasestorage.app",
+  messagingSenderId: "1095601985818",
+  appId: "1:1095601985818:web:8c51a02796795400306354" // ID corregido
 };
 
 const app = initializeApp(firebaseConfig);
